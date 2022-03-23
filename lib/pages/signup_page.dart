e6ee5096c0152bde39ab88dda79a8d7b9c09faa3
Jennifer <rosatuttie@gmail.com>
2022-03-23 00:59:18 -0300

    sign up screen finished

--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:qlorian_app/widgets/appbar.dart';
 
@@ -9,9 +7,11 @@ class SignUpPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
+    IconData lock_outline = const IconData(0xe3b1, fontFamily: 'MaterialIcons');
+
     return Scaffold(
       appBar: const PreferredSize(
-        preferredSize: Size.fromHeight(80),
+        preferredSize: Size.fromHeight(50),
         child: CustomAppBar(),
       ),
       body: SingleChildScrollView(
@@ -61,7 +61,7 @@ class SignUpPage extends StatelessWidget {
                 ],
               ),
             ),
-            const SizedBox(height: 25),
+            const SizedBox(height: 15),
             Padding(
               padding: EdgeInsets.only(left: size.width * 15 / size.width),
               child: Row(
@@ -88,60 +88,218 @@ class SignUpPage extends StatelessWidget {
               ),
             ),
             const SizedBox(
-              height: 35,
+              height: 20,
             ),
             Container(
-              height: size.height * 425 / size.height,
+              height: size.height * 650 / size.height,
               width: size.width * 1440 / size.width,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(200 * 30 / 200),
                 color: Colors.white,
               ),
               child: SingleChildScrollView(
-                  child: Column(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  Padding(
-                    padding:
-                        EdgeInsets.only(left: size.width * 25 / size.width),
-                    child: Row(
-                      children: [
-                        Text(
-                          'Full Name',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: size.width * 50 / 1440),
-                        ),
-                      ],
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(
+                      height: 30,
                     ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(
-                        left: size.width * 25 / size.width,
-                        right: size.width * 25 / size.width),
-                    child: TextFormField(
-                      decoration: InputDecoration(
+                    Padding(
+                      padding:
+                          EdgeInsets.only(left: size.width * 25 / size.width),
+                      child: Row(
+                        children: [
+                          Text(
+                            'Full Name',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: size.width * 50 / 1440),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(
+                          left: size.width * 25 / size.width,
+                          right: size.width * 25 / size.width),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15.0),
+                                borderSide: const BorderSide(
+                                  color: Color(0xff918AE2),
+                                )),
+                            focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15.0),
+                                borderSide: const BorderSide(
+                                  color: Color(0xff918AE2),
+                                )),
+                            prefixIcon: const Icon(
+                              Icons.account_circle_rounded,
+                              color: Color(0xff918AE2),
+                            )),
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    Padding(
+                      padding:
+                          EdgeInsets.only(left: size.width * 25 / size.width),
+                      child: Row(
+                        children: [
+                          Text(
+                            'E-mail',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: size.width * 50 / 1440),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(
+                          left: size.width * 25 / size.width,
+                          right: size.width * 25 / size.width),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15.0),
+                                borderSide: const BorderSide(
+                                  color: Color(0xff918AE2),
+                                )),
+                            focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15.0),
+                                borderSide: const BorderSide(
+                                  color: Color(0xff918AE2),
+                                )),
+                            prefixIcon: const Icon(
+                              Icons.email_outlined,
+                              color: Color(0xff918AE2),
+                            )),
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    Padding(
+                      padding:
+                          EdgeInsets.only(left: size.width * 25 / size.width),
+                      child: Row(
+                        children: [
+                          Text(
+                            'Password',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: size.width * 50 / 1440),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(
+                          left: size.width * 25 / size.width,
+                          right: size.width * 25 / size.width),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15.0),
+                                borderSide: const BorderSide(
+                                  color: Color(0xff918AE2),
+                                )),
+                            focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15.0),
+                                borderSide: const BorderSide(
+                                  color: Color(0xff918AE2),
+                                )),
+                            prefixIcon: Icon(
+                              lock_outline,
+                              color: Color(0xff918AE2),
+                            )),
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    Padding(
+                      padding:
+                          EdgeInsets.only(left: size.width * 25 / size.width),
+                      child: Row(
+                        children: [
+                          Text(
+                            'Confirm Password',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: size.width * 50 / 1440),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(
+                          left: size.width * 25 / size.width,
+                          right: size.width * 25 / size.width),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15.0),
+                              borderSide: const BorderSide(
+                                color: Color(0xff918AE2),
+                              )),
                           focusedBorder: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(15.0),
                               borderSide: const BorderSide(
                                 color: Color(0xff918AE2),
                               )),
-                          prefixIcon: const Icon(
-                            Icons.account_circle_rounded,
-                            color: Color(0xff918AE2),
-                          )),
+                          prefixIcon: Icon(
+                            lock_outline,
+                            color: const Color(0xff918AE2),
+                          ),
+                        ),
+                      ),
                     ),
-                  ),
-                ],
-              )),
-            )
+                    const SizedBox(
+                      height: 55,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(
+                          left: size.width * 25 / size.width,
+                          right: size.width * 25 / size.width),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: ElevatedButton(
+                              style: ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStateProperty.all<Color>(
+                                          const Color(0xff918AE2)),
+                                  shape: MaterialStateProperty.all<
+                                          RoundedRectangleBorder>(
+                                      RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(10.0),
+                                          side: const BorderSide(
+                                              color: Color(0xff918AE2))))
+                                  // foreground
+                                  ),
+                              onPressed: () {},
+                              child: const Text('Sign Up'),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
